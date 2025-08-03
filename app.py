2c7b8ee298dba1eed5855b3a6c7f23d53fa92ff3
SM_SAYEED <sayeed.shahriar@egmail.com>
2025-08-03 18:26:38 +0200

    listening port modified

--- app.py
+++ app.py
@@ -598,4 +598,5 @@ auto_log_material_files()
 
 # ========== MAIN ==========
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(host='0.0.0.0', port=8080)
+    
