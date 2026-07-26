5f7ee5407698d56120d3abc9cf2bcd24509cf5fc
Md. Saidul Islam <126025023+submerged-in-matrix@users.noreply.github.com>
2026-07-26 17:36:38 +0200

    Enable debug mode for the application

--- app.py
+++ app.py
@@ -1912,4 +1912,4 @@ def available_keys():
 
             # ========== MAIN ==========
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
